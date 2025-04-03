b10ea54562b47d1e766c9b5829a45c05267c91f6
github-cloudlabsuser-1348 <168091364+github-cloudlabsuser-1348@users.noreply.github.com>
2025-04-03 17:40:00 +0000

    js celsius conversion exercise

--- skills.js
+++ skills.js
@@ -1,3 +1,10 @@
-function calculateNumbers(var1, var2) {
-  return var1 + var2;
+// funtion to convert Celcius
+// to Fahrenheit
+
+function convertToFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
 }
+
+// Driver code
+let celsius = 25;
+console.log(celsius + "°C is equal to " + convertToFahrenheit(celsius) + "°F");
